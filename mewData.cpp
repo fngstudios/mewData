d133024dd2c9f7d8fb6150c284e13df8638f7f14
fngstudios <ezequielpedace@gmail.com>
2017-07-29 21:27:35 -0300

    Ethernet Begin

--- mewData.cpp
+++ mewData.cpp
@@ -82,11 +82,15 @@ mewData::mewData(EthernetClient* mewClient, const char* Host, const char* Route,
 	_UpdateTime = millis() + _UpdateRate;
     _paramsCount = 0;
 	_debug = 0;
-	DEBUG_MEW("Inicializado");
-	if (!_Client->connect(_Host, _Port)) {
-		DEBUG_MEW("Conexion fallida!");
-	}else{
-		DEBUG_MEW("Conectado!");
+	if (Ethernet.begin(mac) == 0) {
+    	DEBUG_MEW("Failed to configure Ethernet using DHCP");
+  	}else{
+		DEBUG_MEW("Inicializado");
+		if (!_Client->connect(_Host, _Port)) {
+			DEBUG_MEW("Conexion fallida!");
+		}else{
+			DEBUG_MEW("Conectado!");
+		}
 	}
 }
 
